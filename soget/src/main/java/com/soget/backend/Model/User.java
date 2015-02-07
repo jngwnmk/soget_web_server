11a87f3c3e740c3f217b25799df81bee0d5125ee
wonmook <wonmook@wonmook-PC>
2015-02-07 16:47:17 +0900

    Create User class for test

---
+++ soget/src/main/java/com/soget/backend/Model/User.java
@@ -0,0 +1,46 @@
+package com.soget.backend.Model;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+	@Id
+	private Long id;
+	
+	private String name;
+	private String email;
+	
+	public User(){}
+
+	public User(Long id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
